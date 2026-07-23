20b4b0be691b7de39f2595a2d38b11395ea6ffcf
endrowalamsah14-eng <email_githubmu@gmail.com>
2026-07-24 01:07:35 +0700

    System Auto-Update: Real-Time Pipeline Sync

--- app_dashboard.py
+++ app_dashboard.py
@@ -241,8 +241,8 @@ try:
         {"VENDOR": "DHS", "UNIQUE CODE": "Hardblock", "KEY DESTINATION": "BANJARMASIN", "TONASE TARGET (kg)": 0.0, "GROUP DESTINATION": "Banjarmasin DC, Banjarmasin 2 DC, Banjarbaru DC"},
         {"VENDOR": "DHS", "UNIQUE CODE": "Hardblock", "KEY DESTINATION": "BALIKPAPAN", "TONASE TARGET (kg)": 0.0, "GROUP DESTINATION": "Balikpapan DC, Balikpapan 2 DC"},
         {"VENDOR": "Lion Parcel", "UNIQUE CODE": "SV", "KEY DESTINATION": "MEDAN", "TONASE TARGET (kg)": 700.0, "GROUP DESTINATION": "Medan DC, Medan Amplas DC, Percut Sei Tuan DC, Siborong-borong DC, Gunung Sitoli DC"},
-        {"VENDOR": "Lion Parcel", "UNIQUE CODE": "SV", "KEY DESTINATION": "PONTIANAK", "TONASE TARGET (kg)": 1100.0, "GROUP DESTINATION": "Pontianak DC, Pontianak 2 DC, Sungai Kakap DC, Ketapang Kalbar DC"},
-        {"VENDOR": "Lion Parcel", "UNIQUE CODE": "SV", "KEY DESTINATION": "PALANGKA RAYA", "TONASE TARGET (kg)": 1400.0, "GROUP DESTINATION": "Palangka Raya DC, Palangka Raya 2 DC"},
+        {"VENDOR": "Lion Parcel", "UNIQUE CODE": "SV", "KEY DESTINATION": "PONTIANAK", "TONASE TARGET (kg)": 0.0, "GROUP DESTINATION": "Pontianak DC, Pontianak 2 DC, Sungai Kakap DC, Ketapang Kalbar DC"},
+        {"VENDOR": "Lion Parcel", "UNIQUE CODE": "SV", "KEY DESTINATION": "PALANGKA RAYA", "TONASE TARGET (kg)": 0.0, "GROUP DESTINATION": "Palangka Raya DC, Palangka Raya 2 DC"},
         {"VENDOR": "Lion Parcel", "UNIQUE CODE": "SV", "KEY DESTINATION": "BANJARMASIN", "TONASE TARGET (kg)": 4100.0, "GROUP DESTINATION": "Banjarmasin DC, Banjarmasin 2 DC, Banjarbaru DC"},
         {"VENDOR": "Lion Parcel", "UNIQUE CODE": "SV", "KEY DESTINATION": "BALIKPAPAN", "TONASE TARGET (kg)": 3200.0, "GROUP DESTINATION": "Balikpapan DC, Balikpapan 2 DC"}
     ])
